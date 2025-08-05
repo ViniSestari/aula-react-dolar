4e0ac34c1adb03dd339ad6ee4e379f539b9f2819
ViniSestari <vinifujisestari@gmail.com>
2025-08-05 19:44:35 -0300

    add imagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <h1 className='display-3'>Calculadora Dólar</h1>
+      <img src="/public/dollar.png" width='50' />
+
     </>
   )
 }

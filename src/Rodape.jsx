eefd27455bf61e5896e947edf04264ead0dcfd33
ViniSestari <vinifujisestari@gmail.com>
2025-08-05 20:38:26 -0300

    Criação Componente Footer

---
+++ src/Rodape.jsx
@@ -0,0 +1,20 @@
+import './Rodape.css'
+
+function Rodape() {
+    return (
+        <>
+            <footer className='text-center bg-dark text-white p-3'>
+                <p>
+                    Desenvolvido por &nbsp;
+                    <a href='https://linkedin.com/in/Vinicius-Sestari'
+                    target='blank' className='text-danger'>Vinicius Sestari</a>
+                    &nbsp; no &nbsp;
+                    <a href='https://sp.senac.br/americana'
+                    target='blank' className='text-danger'>Senac Americana</a>
+                </p>
+            </footer>
+        </>
+    )
+}
+
+export default Rodape
